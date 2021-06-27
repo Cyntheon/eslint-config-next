5d371b5c677800db81ec7cfeddfc2a915d4566a7
Cynthia Chung <cynthiachung03@gmail.com>
2021-06-27 16:42:53 -0400

    Use external preset for react-less typescript

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ module.exports = {
     "node": true
   },
   "extends": [
-    "eslint:recommended",
+    "@cyntheon/typescript",
     "next",
     "next/core-web-vitals",
-    "google"
   ],
   "globals": {
     "JSX": true,
@@ -16,48 +15,10 @@ module.exports = {
   "ignorePatterns": [
     "next-env.d.ts"
   ],
-  "plugins": [
-    "prefer-arrow"
-  ],
   "rules": {
-    // https://github.com/TristonJ/eslint-plugin-prefer-arrow
-    "prefer-arrow/prefer-arrow-functions": [
-      "error",
-      {
-        "disallowPrototype": true
-      }
-    ],
     "jsx-quotes": [
       "error",
       "prefer-double"
-    ],
-    "quotes": [
-      "error",
-      "double",
-      {
-        "allowTemplateLiterals": true
-      }
-    ],
-    "capitalized-comments": [
-      "error",
-      "always",
-      {
-        "ignoreConsecutiveComments": true
-      }
-    ],
-    "indent": [
-      "error",
-      2
-    ],
-    "require-jsdoc": "off",
-    "valid-jsdoc": "off",
-    "semi-style": [
-      "error",
-      "last"
-    ],
-    "comma-dangle": [
-      "error",
-      "never"
     ],
     // https://github.com/yannickcr/eslint-plugin-react
     "react/button-has-type": "error",
